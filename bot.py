51073350a63968df004ccd6481a943fb1028b1af
usdtpay3-eng <usdtpay3@gmail.com>
2026-07-24 14:23:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -219,7 +219,13 @@ from telegram.ext import Application, CommandHandler, MessageHandler, Conversati
 
 (NAME, MOBILE, CAP1, OTP1, CAP2, OTP2) = range(6)
 
-BOT_TOKEN = 8870070946:AAFv_6DD0_7o37KDYEKtC7D9MhhE9wKY2u8
+def load_token():
+    token = os.getenv("BOT_TOKEN")
+
+    if not token:
+        raise ValueError("BOT_TOKEN environment variable not found")
+
+    return token
 
 class UserSession:
     def __init__(self):
